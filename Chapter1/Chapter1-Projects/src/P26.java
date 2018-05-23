97b06d95589894e9a7133837efe01231f8e82f99
Feazan <feazan.yaseen@stonybrook.edu>
2018-05-23 16:32:55 -0400

    Starting question P-1.26

---
+++ Chapter1/Chapter1-Projects/src/P26.java
@@ -0,0 +1,17 @@
+/**
+ * Solution for Data Structures and Algorithms 6th edition --
+ * Creativity P-1.26
+ *
+ * Write a short Java program that takes all the lines input to standard input and
+ * writes them to standard output in reverse order. That is, each line is output in the
+ * correct order, but the ordering of the lines is reversed.
+ *
+ * @author Feazan Yaseen
+ */
+public class P26
+{
+    private static String reverseLine(String line)
+    {
+        return "";
+    }
+}
